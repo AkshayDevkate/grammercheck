d49603cce156f834a06d8c9394b9ca9ac1b5a4dd
Akshay Devkate <devkatte.akshay98@gmail.com>
2022-03-29 11:29:24 +0200

    Change in question

---
+++ Downloads/finalmvp/src/App.js
@@ -0,0 +1,26 @@
+
+import * as React from 'react';
+import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import UserForm from './components/UserForm';
+
+
+
+;
+
+
+
+function App() {
+
+
+  return (
+<>
+<Header></Header>
+<UserForm></UserForm>   
+<Footer></Footer>
+   </>
+  );
+}
+
+export default App;
